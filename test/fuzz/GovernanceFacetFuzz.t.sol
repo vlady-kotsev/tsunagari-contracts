156294ee7960f4cee991e8e436bcf7f594b6360a
vlady-kotsev <vladislav.kotsev@limechain.tech>
2024-10-29 17:11:31 +0200

    fix: fix fuzz test

--- test/fuzz/GovernanceFacetFuzz.t.sol
+++ test/fuzz/GovernanceFacetFuzz.t.sol
@@ -35,7 +35,8 @@ contract GovernanceFacetFuzzTest is Test, SignatureGenerator {
         members[2] = m3;
 
         vm.assume(m1 != address(0) && m2 != address(0) && m3 != address(0));
-
+        vm.assume(m1 != m2 && m1 != m3 && m2 != m3);
+        
         governanceFacet.initGovernance(members, 2);
         assertEq(diamond.getThreshold(), 1);
     }
